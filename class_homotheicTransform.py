5f0f6a7eb11fc104dd7ea90706eaedf722b94e9c
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-03-13 17:03:03 -0400

    moved homotheicTransform test code to run.py

--- class_homotheicTransform.py
+++ class_homotheicTransform.py
@@ -24,44 +24,10 @@ class homotheicTransform():
         return C
     
     def mask_time_interval(self, exp, var):
+        # verbose rename
         lp = self.ttu[:,:,2,:]
         slice_mask_lp = np.apply_along_axis(lambda x: exp - var < np.linalg.norm(x) < exp + var, 1, lp)
         full_mask_lp = np.stack([slice_mask_lp for i in range(lp.shape[1])], axis = 1).astype('int')
         import pdb; pdb.set_trace()
         return self.ttu[:,:,0,:] * full_mask_lp
 
-
-if __name__ == '__main__':
-    obs = observer(np.array([0, 0, 0]))
-    el = elipsiod(np.array([1/np.sqrt(3), 1/np.sqrt(3), 1/np.sqrt(3)]),
-                  0,
-                  np.array([.55, .25, .1]),
-                  np.array([1, 3, 2]),
-                  np.array([40, 0, 0]),
-                  .25)
-    hT = homotheicTransform(obs, el)
-
-    import matplotlib.pyplot as plt
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    def construct_(ttu, index):
-        xs = ttu[:,0,index,:].flatten()
-        ys = ttu[:,1,index,:].flatten()
-        zs = ttu[:,2,index,:].flatten()
-        
-        return xs, ys, zs
-
-    xs0, ys0, zs0 = construct_(hT.ttu, 0)
-    xs2, ys2, zs2 = construct_(hT.ttu, 2)
-    xs3, ys3, zs3 = hT.obs.location[0], hT.obs.location[1], hT.obs.location[2]
-    ax.scatter(xs2, ys2, zs2)
-    ax.scatter(xs0, ys0, zs0)
-    ax.scatter(xs3, ys3, zs3)
-    plt.show()
-
-    def plot_transformed_obj(mti):
-        K = [mti[:,i,:].flatten() for i in range(mti.shape[1])]
-        ax.scatter(*K)
-        plt.show()
--- run.py
+++ run.py
@@ -3,11 +3,37 @@ from class_observer import observer
 from class_homotheicTransform import homotheicTransform
 import numpy as np
 
-obs = observer(np.array([3, 1, 5]))
-el = elipsiod(np.array([1/np.sqrt(3), 1/np.sqrt(3), 1/np.sqrt(3)]),
-              0,
-              np.array([1, .25, 2]),
-              np.array([1, 3, 2]),
-              np.array([-.5, -.5, -.5]),
-              .25)
-hT = homotheicTransform(obs, el)
+if __name__ == '__main__':
+    # look to make these plot functions methods of the class
+    obs = observer(np.array([0, 0, 0]))
+    el = elipsiod(np.array([1/np.sqrt(3), 1/np.sqrt(3), 1/np.sqrt(3)]),
+                  0,
+                  np.array([.55, .25, .1]),
+                  np.array([1, 3, 2]),
+                  np.array([40, 0, 0]),
+                  .25)
+    hT = homotheicTransform(obs, el)
+
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    def construct_(ttu, index):
+        xs = ttu[:,0,index,:].flatten()
+        ys = ttu[:,1,index,:].flatten()
+        zs = ttu[:,2,index,:].flatten()
+        return xs, ys, zs
+
+    xs0, ys0, zs0 = construct_(hT.ttu, 0)
+    xs2, ys2, zs2 = construct_(hT.ttu, 2)
+    xs3, ys3, zs3 = hT.obs.location[0], hT.obs.location[1], hT.obs.location[2]
+    ax.scatter(xs2, ys2, zs2)
+    ax.scatter(xs0, ys0, zs0)
+    ax.scatter(xs3, ys3, zs3)
+    plt.show()
+
+    def plot_transformed_obj(mti):
+        K = [mti[:,i,:].flatten() for i in range(mti.shape[1])]
+        ax.scatter(*K)
+        plt.show()
